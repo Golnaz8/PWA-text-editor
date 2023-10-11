579cda18ac6d02de6e0d145e51d37d25b225a3d9
Golnaz8 <golnaz.brj@gmail.com>
2023-10-10 22:30:15 -0400

    debugged an error inside database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,37 +14,28 @@ const initdb = async () =>
 
 
 // Add logic to a method that accepts some content and adds it to the database
-export const putDb = async ( content) => {
-  try {
-    console.log('PUT to the database', content);
-    const jateDb = await openDB('jate', 1);
-    const tx = jateDb.transaction('jate', 'readwrite');
-    const store = tx.objectStore('jate');
-    const request = store.put({ id: 1, value : content });
-    const result = await request;
-    console.log('adds content to the database', result);
-    return result;
-  } catch (error) {
-    console.error('putDb not implemented', error);
-  }
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Adds content to the database', result.value);
 };
 
-
-// Add logic for a method that gets all the content from the database
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
-  try {
-    console.log('GET all from the database');
-    const jateDb = await openDB('jate', 1);
-    const tx = jateDb.transaction('jate', 'readonly');
-    const store = tx.objectStore('jate');
-    const request = store.get(1);
-    const result = await request;
-    console.log('gets all the content from the database', result);
-    return result?.value;
-  } catch (error) {
-    console.error('getDb not implemented', error);
-  }
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('Gets all the content from the database', result.value)
+    : console.log('Something went wrong!!');
+  return result?.value;
 };
-
 
 initdb();
